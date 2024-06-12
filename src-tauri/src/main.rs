6cc4d8e9b58cd6d923a50e4e2cc5a697e09e9738
Vlad Tanasie <47207809+tanasievlad246@users.noreply.github.com>
2024-06-12 09:31:47 +0300

    releasing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,12 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// #[tauri::command]
+// fn create_local_error_log() {}
+
+// #[tauri::command]
+// fn write_to_local_error_log() {}
+
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
